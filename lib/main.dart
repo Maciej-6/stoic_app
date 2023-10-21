8daaa88bf42f51586cb35724395b28b746dbbc33
Maciej-6 <maciej.gauza@gmail.com>
2023-10-21 20:38:40 +0200

    error

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stoic_app/app/pages/home_page/sub_pages/quotes_page/cubit/quotes_cubit.dart';
-import 'package:stoic_app/http.dart';
-
-import 'app/pages/home_page/home_page.dart';
+import 'package:stoic_app/app/pages/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
-  HttpOverrides.global = MyHttpOverrides();
 }
 
 class MyApp extends StatelessWidget {
