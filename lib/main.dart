e4c6ce7fb8526387eef21567280ce616b28f28e7
Michal Gauza <your_email@example.com>
2023-10-22 00:31:31 +0200

    task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'app/pages/home_page/home_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,4 +17,77 @@ class MyApp extends StatelessWidget {
       home: const HomePage(),
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController input1Controller = TextEditingController();
+  final TextEditingController input2Controller = TextEditingController();
+
+  // jakie są różnice między tymi typami zmiennych
+  // late final double result;
+  // var result;
+  double? result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: input1Controller,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                const Text('+'),
+                Expanded(
+                  child: TextField(
+                    controller: input2Controller,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                const Text('='),
+                Expanded(
+                  child: Text('${result}'), // zrób tak że jeżeli result jest nullem to wyświetli się pusty string ('')
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final num1 = double.parse(input1Controller.text);
+                final num2 = double.parse(input2Controller.text);
+                // zrób tak żeby ta funkcja działała
+                add()
+              },
+              child: const Text('add'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // napisz analogiczną funjcę z odejmowaniem
+  void add(double num1, double num2,) {
+    setState(() {
+      result = num1 + num2;
+    });
+  }
+
+  // napisz analogiczne funkcje ale z 3 parametrami
 }
